791c36f48d46bd7611d44b6d1036a521fccbd935
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-11-26 22:45:47 +0800

    Project File Creation
    part 2

---
+++ BankingManagementSystem/src/main/java/com/mycompany/bankingmanagementsystem/BankingManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bankingmanagementsystem;
+
+/**
+ *
+ * @author Jayjay
+ */
+public class BankingManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
